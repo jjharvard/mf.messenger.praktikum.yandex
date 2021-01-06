975c4a3ecff160739ae9f2682d6454d2421e75d5
jjharvard <jjharvard@yandex.ua>
2021-01-06 09:14:10 +0300

    Add server

---
+++ server/app.js
@@ -0,0 +1,13 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+
+const PORT = 3000
+const STATIC_FILES_DIR = path.join(__dirname, '../dist')
+
+app
+    .use(express.static(STATIC_FILES_DIR))
+    .listen(PORT, () => {
+        console.log(`Listening on port ${PORT}`)
+    })
+
