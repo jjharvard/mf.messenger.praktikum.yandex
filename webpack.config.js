4c5ee58f8aba0e6ecff91401379865ab65b0914a
jjharvard <jjharvard@yandex.ua>
2021-02-22 11:56:12 +0300

    Add assets

--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src', 'index.html')
+            template: path.resolve(__dirname, 'assets', 'index.html')
         })
     ]
 };
