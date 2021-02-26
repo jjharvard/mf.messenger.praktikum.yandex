8cd1b129d7301db1c22d6898668a5e355557e3ec
jjharvard <jjharvard@yandex.ua>
2021-02-26 04:39:57 +0300

    Remove log

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ app
         contentSecurityPolicy: false,
     }))
     .use(express.static(STATIC_FILES_DIR), function (req, res, next) {
-        console.log(req.url);
         let re = /\/[a-z-]+$/;
         if (re.test(req.url)) {
             res.sendFile(path.resolve(STATIC_FILES_DIR, 'index.html'));
